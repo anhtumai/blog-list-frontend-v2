d188a90f25e7fbddf72de771157944f1e717ac18
anhtumai <tu.mai@outlook.com>
2021-08-10 11:20:36 +0300

    Add sort blogs by likes

--- src/selectors/index.ts
+++ src/selectors/index.ts
@@ -2,6 +2,7 @@ export const userSelector = (state: RootState) => state.user;
 
 export const notificationSelector = (state: RootState) => state.notification;
 
-export const blogsSelector = (state: RootState) => state.blogs;
+export const blogsSelector = (state: RootState) =>
+  state.blogs.sort((a, b) => b.likes - a.likes);
 
 export const usersSelector = (state: RootState) => state.users;
