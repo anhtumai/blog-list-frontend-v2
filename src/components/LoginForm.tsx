94155956d4b1d2b9d2460b1101ec2739e78a596a
anhtumai <tu.mai@outlook.com>
2021-08-14 00:25:42 +0300

    Decorate LoginForm

--- src/components/LoginForm.tsx
+++ src/components/LoginForm.tsx
@@ -1,11 +1,56 @@
+import {
+  TextField,
+  Button,
+  makeStyles,
+  Typography,
+  Avatar,
+  Grid,
+  Paper,
+} from "@material-ui/core";
+
+import LockOutlinedIcon from "@material-ui/icons/LockOutlined";
+
 import { useDispatch } from "react-redux";
 
 import { useInputField } from "../hooks";
 
 import { startLogin } from "../actions/auth";
 
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "center",
+    padding: theme.spacing(2),
+
+    "& .MuiTextField-root": {
+      margin: theme.spacing(1),
+      width: "300px",
+    },
+    "& .MuiButtonBase-root": {
+      margin: theme.spacing(2),
+    },
+  },
+  avatar: {
+    margin: "auto",
+    background: "#1bbd7e",
+  },
+  paper: {
+    padding: 20,
+    height: "70vh",
+    width: 280,
+    margin: "auto",
+  },
+  typography: {
+    fontSize: "1.5em",
+    marginTop: "2vh",
+  },
+}));
+
 const LoginForm = () => {
   const dispatch = useDispatch();
+  const classes = useStyles();
 
   const username = useInputField();
   const password = useInputField();
@@ -22,33 +67,45 @@ const LoginForm = () => {
   }
 
   return (
-    <div>
-      <h2>login to application</h2>
-
-      <form onSubmit={handleLogin}>
-        <div>
-          username
-          <input
-            data-testid="username"
+    <Grid>
+      <Paper className={classes.paper}>
+        <form className={classes.root} onSubmit={handleLogin}>
+          <Grid>
+            <Avatar className={classes.avatar}>
+              <LockOutlinedIcon />
+            </Avatar>
+            <Typography
+              variant="h2"
+              component="h2"
+              className={classes.typography}
+            >
+              Sign in
+            </Typography>
+          </Grid>
+          <TextField
+            label="Username"
+            variant="filled"
+            required
             value={username.value}
             onChange={username.onChange}
           />
-        </div>
-
-        <div>
-          password
-          <input
-            data-testid="password"
+          <TextField
+            label="Password"
+            variant="filled"
             type="password"
+            required
             value={password.value}
             onChange={password.onChange}
           />
-        </div>
-        <button data-testid="login-btn" type="submit">
-          login
-        </button>
-      </form>
-    </div>
+
+          <div>
+            <Button type="submit" variant="contained" color="primary">
+              Login
+            </Button>
+          </div>
+        </form>
+      </Paper>
+    </Grid>
   );
 };
 
