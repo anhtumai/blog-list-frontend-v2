ee2e2da85b98a3b525fd257ac23fd86708b1762b
anhtumai <tu.mai@outlook.com>
2021-08-15 18:17:54 +0300

    Decorate comment section

--- src/components/CommentsSection.tsx
+++ src/components/CommentsSection.tsx
@@ -1,46 +1,95 @@
 import { useDispatch, useSelector } from "react-redux";
+import {
+  Paper,
+  Typography,
+  Button,
+  TextField,
+  makeStyles,
+  Grid,
+  List,
+  ListItem,
+  ListItemIcon,
+  ListItemText,
+} from "@material-ui/core";
+
+import CommentIcon from "@material-ui/icons/Comment";
+
 import { useInputField } from "../hooks";
 
 import { startCreateComment } from "../actions/comment";
 import { userSelector } from "../selectors";
 
+const useStyles = makeStyles((theme) => ({
+  root: {
+    marginTop: "5vh",
+  },
+  form: {
+    marginTop: "2vh",
+  },
+  button: {
+    marginBottom: 0,
+  },
+}));
+
 const CommentsSection = ({ blog }: { blog: IBlog }) => {
   const dispatch = useDispatch();
+  const classes = useStyles();
 
   const user = useSelector(userSelector) as IUserWithToken;
 
   const content = useInputField();
 
-  const liStyle = {
-    marginTop: "10px",
-  };
-
-  async function onSubmit(event: React.SyntheticEvent) {
+  async function handleSubmit(event: React.SyntheticEvent) {
     event.preventDefault();
     const submittedInfo = { content: content.value };
     content.reset();
     dispatch(startCreateComment(blog.id, submittedInfo, user.token));
   }
   return (
-    <div>
-      <h3>comments</h3>
-      <form onSubmit={onSubmit}>
-        <input
-          id="content"
-          type="text"
-          value={content.value}
-          onChange={content.onChange}
-        />
-        <button type="submit">add comment</button>
+    <Paper className={classes.root}>
+      <Typography component="h6" variant="h6">
+        comments
+      </Typography>
+      <form onSubmit={handleSubmit} className={classes.form}>
+        <Grid
+          container
+          item
+          direction="row"
+          justifyContent="flex-start"
+          alignItems="center"
+          xs={12}
+        >
+          <TextField
+            required
+            variant="outlined"
+            size="small"
+            label="comment"
+            type="text"
+            value={content.value}
+            onChange={content.onChange}
+          />
+          <Button
+            size="small"
+            variant="text"
+            color="primary"
+            type="submit"
+            className={classes.button}
+          >
+            add comment
+          </Button>
+        </Grid>
       </form>
-      <ul>
+      <List component="ul" dense={true}>
         {blog.comments.map((comment) => (
-          <li style={liStyle} key={comment.id}>
-            {comment.content}
-          </li>
+          <ListItem key={comment.id}>
+            <ListItemIcon>
+              <CommentIcon />
+            </ListItemIcon>
+            <ListItemText>{comment.content}</ListItemText>
+          </ListItem>
         ))}
-      </ul>
-    </div>
+      </List>
+    </Paper>
   );
 };
 
